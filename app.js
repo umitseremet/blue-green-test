405d5bffe6633df139891c2a02d6c26ac0c6194b
umitseremet <useremet@hotmail.com>
2021-01-02 23:36:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 80
 
-app.get('/', (req, res) => res.send('Hello World! this is v5.7'))
+app.get('/', (req, res) => res.send('Hello World! this is v10.0'))
 app.get('/health', (req, res) => res.send('Health check status: ok'))
 
 app.listen(80, () => console.log(`Example app listening at http://localhost:${port}`))
